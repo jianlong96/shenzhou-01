0db982e3fedfebe8f410a482bfbea32281ce95df
jianlong96 <liujianlong_hw_hrb@163.com>
2022-10-12 15:54:55 +0800

    reset base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base:'/githubpages-demo-1/dist/',
+    // base:'/githubpages-demo-1/dist/',
     plugins: [vue()]
 })
