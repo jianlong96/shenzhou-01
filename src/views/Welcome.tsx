101207d6a04815e8a523d0ef02d817505ba9c95d
jianlong96 <liujianlong_hw_hrb@163.com>
2022-10-12 22:05:20 +0800

    创建欢迎界面的页面

---
+++ src/views/Welcome.tsx
@@ -0,0 +1,11 @@
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+export const Welcome = defineComponent({
+    setup: (props, context) => {
+        return () => <>
+            <header>logo</header>
+            <main><RouterView /></main>
+            <footer>buttons</footer>
+        </>
+    }
+})
