8c652b8e1aa469e8a61fd60973824d23e1b7d02f
jianlong96 <liujianlong_hw_hrb@163.com>
2022-10-17 20:01:08 +0800

    完成四个欢迎页面

--- src/components/welcome/first.tsx
+++ src/components/welcome/first.tsx
@@ -8,7 +8,7 @@ export const First = defineComponent({
                 <div class={s.card}>
                     <img class={s.pig} src={pig} />
                     <h2>会挣钱<br/>还要会省钱</h2>
-                </div>
+                </div> 
                 <div class={s.actions}>
                     <RouterLink class={s.fake} to="/start">跳过</RouterLink>
                     <RouterLink to="/welcome/2">下一页</RouterLink>
--- src/components/welcome/forth.tsx
+++ src/components/welcome/forth.tsx
@@ -1,8 +1,20 @@
 import { defineComponent } from 'vue';
+import s from './First.module.scss';
+import clound from '../../assets/icons/clound.svg'
 export const Forth = defineComponent({
     setup: (props, context) => {
         return () => (
-            <div>Forth</div>
+            <div class={s.wrapper}>
+                <div class={s.card}>
+                    <img class={s.clound} src={clound} />
+                    <h2>云备份<br />再也不怕数据丢失</h2>
+                </div>
+                <div class={s.actions}>
+                    <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+                    <RouterLink to="/start">完成</RouterLink>
+                    <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+                </div>
+            </div >
         )
     }
-})
+}) 
