5a343a832e712c5af9b75e91b434f299b6e0f8dd
jianlong96 <liujianlong_hw_hrb@163.com>
2022-10-17 21:37:06 +0800

    提取 WelcomeLayout 组件

--- src/components/welcome/third.tsx
+++ src/components/welcome/third.tsx
@@ -1,20 +1,22 @@
 import { defineComponent } from 'vue';
 import s from './First.module.scss';
 import echart from '../../assets/icons/echart.svg';
+import { WelcomeLayout } from './WelcomeLayout';
+import { RouterLink } from 'vue-router';
 export const Third = defineComponent({
     setup: (props, context) => {
         return () => (
-            <div class={s.wrapper}>
-                <div class={s.card}>
-                    <img class={s.echart} src={echart} />
-                    <h2>数据可视化<br />收支一目了然</h2>
-                </div>
-                <div class={s.actions}>
-                    <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-                    <RouterLink to="/welcome/4">下一页</RouterLink>
-                    <RouterLink to="/start">跳过</RouterLink>
-                </div>
-            </div >
+            <WelcomeLayout>
+                {{
+                    icon: () => <img class={s.echart} src={echart} />,
+                    title: () => <h2>数据可视化<br />收支一目了然</h2>,
+                    buttons: () => <>
+                        <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+                        <RouterLink to="/welcome/4">下一页</RouterLink>
+                        <RouterLink to="/start">跳过</RouterLink>
+                    </>
+                }}
+            </WelcomeLayout >
         )
     }
-}) 
+})
