3d84d5c31e94bee1c50090298b50dbf7b03827f8
jianlong96 <liujianlong_hw_hrb@163.com>
2022-10-18 14:57:55 +0800

    换一种方法重构

--- src/components/welcome/forth.tsx
+++ src/components/welcome/forth.tsx
@@ -1,22 +1,19 @@
-import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './WelcomeLayout.module.scss';
 import { RouterLink } from 'vue-router';
 import { WelcomeLayout } from './WelcomeLayout';
 import clound from '../../assets/icons/clound.svg'
-export const Forth = defineComponent({
-    setup: (props, context) => {
-        return () => (
-            <WelcomeLayout>
-                {{
-                    icon: () => <img class={s.clound} src={clound} />,
-                    title: () => <h2>云备份<br />再也不怕数据丢失</h2>,
-                    buttons: () => <>
-                        <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-                        <RouterLink to="/start">完成</RouterLink>
-                        <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-                    </>
-                }}
-            </WelcomeLayout>
-        )
-    }
-})
+export const Forth = () => (
+    <WelcomeLayout>
+        {{
+            icon: () => <img class={s.icon} src={clound} />,
+            title: () => <h2>云备份<br />再也不怕数据丢失</h2>,
+            buttons: () => <>
+                <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+                <RouterLink to="/start" >完成</RouterLink>
+                <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+            </>
+        }}
+    </WelcomeLayout>
+)
+
+Forth.displayName = 'Forth'
--- src/components/welcome/third.tsx
+++ src/components/welcome/third.tsx
@@ -1,22 +1,21 @@
-import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './WelcomeLayout.module.scss';
 import echart from '../../assets/icons/echart.svg';
 import { WelcomeLayout } from './WelcomeLayout';
 import { RouterLink } from 'vue-router';
-export const Third = defineComponent({
-    setup: (props, context) => {
-        return () => (
-            <WelcomeLayout>
-                {{
-                    icon: () => <img class={s.echart} src={echart} />,
-                    title: () => <h2>数据可视化<br />收支一目了然</h2>,
-                    buttons: () => <>
-                        <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-                        <RouterLink to="/welcome/4">下一页</RouterLink>
-                        <RouterLink to="/start">跳过</RouterLink>
-                    </>
-                }}
-            </WelcomeLayout >
-        )
-    }
-})
+export const Third = () => {
+    return (
+        <WelcomeLayout>
+            {{
+                icon: () => <img class={s.icon} src={echart} />,
+                title: () => <h2>数据可视化<br />收支一目了然</h2>,
+                buttons: () => <>
+                    <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+                    <RouterLink to="/welcome/4" >下一页</RouterLink>
+                    <RouterLink to="/start" >跳过</RouterLink>
+                </>
+            }}
+        </WelcomeLayout>
+    )
+}
+
+Third.displayName = 'Third'
