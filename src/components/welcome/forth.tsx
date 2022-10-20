603eff4c4da7ca47168a7edba2586e9d2b32ea8b
jianlong96 <liujianlong_hw_hrb@163.com>
2022-10-20 09:25:59 +0800

    使用 svg sprites

--- src/components/welcome/forth.tsx
+++ src/components/welcome/forth.tsx
@@ -1,8 +1,9 @@
 import s from './Welcome.module.scss'
-import clound from '../../assets/icons/clound.svg'
 export const Forth = () => (
     < div class={s.card}>
-        <img class={s.icon} src={clound} />
+        <svg>
+            <use xlinkHref='#clound'></use>
+        </svg>
         <h2>云备份<br />再也不怕数据丢失</h2>
     </div>
 )
--- src/components/welcome/third.tsx
+++ src/components/welcome/third.tsx
@@ -1,12 +1,12 @@
 import s from './Welcome.module.scss'
-import echart from '../../assets/icons/echart.svg';
 export const Third = () => {
     return (
         < div class={s.card}>
-            <img class={s.icon} src={echart} />
+            <svg>
+                <use xlinkHref='#echart'></use>
+            </svg>
             <h2>数据可视化<br />收支一目了然</h2>
         </div>
     )
-}
-
+} 
 Third.displayName = 'Third'
