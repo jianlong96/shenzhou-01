16555130cc334171ce8bfe88a17773f58277ffca
jianlong96 <liujianlong_hw_hrb@163.com>
2022-10-20 10:12:13 +0800

    支持touch事件（addEventListen is function）

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,14 @@
-import { defineComponent, h, Transition, VNode } from 'vue';
 import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
+import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue';
+import { useSwipe } from '../hooks/useSwipe';
 import s from './Welcome.module.scss'
-import logo from '../assets/icons/logo.svg'
 export const Welcome = defineComponent({
-
     setup: (props, context) => {
+        const main = ref<HTMLElement | null>(null)
+        const { direction, swiping } = useSwipe(main)
+        watchEffect(() => {
+        })
+
         return () => <div class={s.wrapper}>
             <header>
                 <svg>
@@ -13,7 +17,7 @@ export const Welcome = defineComponent({
                 <h1>山竹记账</h1>
             </header>
             <main class={s.main}>
-                <RouterView name="main">
+                <RouterView name="main" ref={main}>
                     {({ Component: X, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
                         <Transition enterFromClass={s.slide_fade_enter_from} enterActiveClass={s.slide_fade_enter_active}
                             leaveToClass={s.slide_fade_leave_to} leaveActiveClass={s.slide_fade_leave_active}>
