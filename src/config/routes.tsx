fb3257211f684ad00dc454700b0551f671cb49ef
jianlong96 <liujianlong_hw_hrb@163.com>
2022-10-23 14:04:19 +0800

    创建 /items 和 /items/create 路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -9,6 +9,9 @@ import { Third } from '../components/welcome/Third'
 import { ThirdActions } from "../components/welcome/ThirdActions";
 import { Welcome } from '../views/Welcome';
 import { StartPage } from '../views/StartPage';
+import { ItemPage } from '../views/ItemPage';
+import { ItemList } from '../views/ItemList';
+import { ItemCreate } from '../views/ItemCreate';
 
 export const routes: RouteRecordRaw[] = [
     { path: '/', redirect: '/welcome/1' },
@@ -23,4 +26,11 @@ export const routes: RouteRecordRaw[] = [
         ],
     },
     { path: '/start', component: StartPage },
+    {
+        path: '/items', component: ItemPage,
+        children: [
+            { path: '', component: ItemList },
+            { path: 'create', component: ItemCreate },
+        ]
+    }
 ]
