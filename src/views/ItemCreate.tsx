70f35a93ddef2c82672b13a2d1d042304c9622ba
jianlong96 <liujianlong_hw_hrb@163.com>
2022-10-26 10:20:28 +0800

    实现 Tabs 功能

--- src/views/ItemCreate.tsx
+++ src/views/ItemCreate.tsx
@@ -1,6 +1,7 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import { MainLayout } from '../layouts/Mainlayout';
 import { Icon } from '../shared/Icon';
+import { Tab, Tabs } from '../shared/Tabs';
 import s from './ItemCreate.module.scss';
 export const ItemCreate = defineComponent({
     props: {
@@ -9,16 +10,22 @@ export const ItemCreate = defineComponent({
         }
     },
     setup: (props, context) => {
+        const refKind = ref('支出')
         return () => (
-            <MainLayout>{
-                {
-                    title: () => '记一笔',
-                    icon: () => <Icon name='left' class={s.navIcon} />,
-                    default: () => <>
-                        <div>main</div>
-                    </>
-                }
-            }</MainLayout>
+            <MainLayout>{{
+                title: () => '记一笔',
+                icon: () => <Icon name='left' class={s.navIcon} />,
+                default: () => <>
+                    <Tabs selected={refKind.value} onUpdateSelected={name => refKind.value = name}>
+                        <Tab name="支出">
+                            Icon列表1
+                        </Tab>
+                        <Tab name="收入">
+                            Icon列表2
+                        </Tab>
+                    </Tabs>
+                </>
+            }}</MainLayout>
         )
     }
 })
