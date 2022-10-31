2c4033619d2738dacf0fbcbd50783a06221dd8b2
jianlong96 <liujianlong_hw_hrb@163.com>
2022-10-31 16:11:22 +0800

    完成 TagEdit 页面的样式

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,13 +1,22 @@
-import { defineComponent } from "vue";
+import { defineComponent, PropType } from "vue";
 import s from './Button.module.scss';
 
 interface Props {
-    onClick?: (e: MouseEvent) => void
 }
-export const Button = defineComponent<Props>({  // <Props>泛型
+export const Button = defineComponent({
+    props: {
+        onClick: {
+            type: Function as PropType<(e: MouseEvent) => void>
+        },
+        level: {
+            type: String as PropType<'important' | 'normal' | 'danger'>,
+            default: 'important'
+        }
+    },
     setup: (props, context) => {
+
         return () => (
-            <button class={s.button}>
+            <button class={[s.button, s[props.level]]}>
                 {context.slots.default?.()}
             </button>
         )
