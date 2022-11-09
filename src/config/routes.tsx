4b11478cdbbc2a1675c108e0818b29d532c76576
jianlong96 <liujianlong_hw_hrb@163.com>
2022-11-09 20:49:10 +0800

    每次切换tab时重新渲染图表

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,58 +1,61 @@
-import { RouteRecordRaw } from 'vue-router'
-import { First } from '../components/welcome/First'
+import { RouteRecordRaw } from "vue-router";
+import { First } from "../components/welcome/First";
 import { FirstActions } from "../components/welcome/FirstActions";
-import { Forth } from '../components/welcome/Forth'
+import { Forth } from "../components/welcome/Forth";
 import { TagCreate } from "../components/tag/TagCreate";
 import { TagEdit } from "../components/tag/TagEdit";
 import { ForthActions } from "../components/welcome/ForthActions";
-import { Second } from '../components/welcome/Second'
+import { Second } from "../components/welcome/Second";
 import { SecondActions } from "../components/welcome/SecondActions";
-import { Third } from '../components/welcome/Third'
+import { Third } from "../components/welcome/Third";
 import { ThirdActions } from "../components/welcome/ThirdActions";
-import { Welcome } from '../views/Welcome';
-import { StartPage } from '../views/StartPage';
-import { ItemPage } from '../views/ItemPage';
-import { ItemList } from '../views/ItemList';
-import { ItemCreate } from '../views/ItemCreate';
+import { Welcome } from "../views/Welcome";
+import { StartPage } from "../views/StartPage";
+import { ItemPage } from "../views/ItemPage";
+import { ItemList } from "../views/ItemList";
+import { ItemCreate } from "../views/ItemCreate";
 import { TagPage } from "../views/TagPage";
-import { SignInPage } from '../views/SignInPage';
+import { SignInPage } from "../views/SignInPage";
 import { http } from "../shared/Http";
-import { StatisticsPage } from '../views/StatisticsPage';
+import { StatisticsPage } from "../views/StatisticsPage";
 export const routes: RouteRecordRaw[] = [
-    { path: '/', redirect: '/welcome/1' },
-    {
-        path: '/welcome',
-        component: Welcome,
-        beforeEnter: (to, from, next) => {
-            localStorage.getItem('skipFeatures') === 'yes' ? next('/start') : next()
-        },
-        children: [
-            { path: '1', components: { main: First, footer: FirstActions }, },
-            { path: '2', components: { main: Second, footer: SecondActions }, },
-            { path: '3', components: { main: Third, footer: ThirdActions }, },
-            { path: '4', components: { main: Forth, footer: ForthActions }, },
-        ],
+  { path: "/", redirect: "/welcome/1" },
+  {
+    path: "/welcome",
+    component: Welcome,
+    beforeEnter: (to, from, next) => {
+      localStorage.getItem("skipFeatures") === "yes" ? next("/start") : next();
     },
-    { path: '/start', component: StartPage },
-    {
-        path: '/items', component: ItemPage,
-        children: [
-            { path: '', component: ItemList },
-            { path: 'create', component: ItemCreate },
-        ]
-    },
-    {
-        path: '/tags', component: TagPage,
-        children: [
-            { path: 'create', component: TagCreate },
-            { path: ':id/edit', component: TagEdit }
-        ]
-    },
-    {
-        path: '/sign_in', component: SignInPage
-    },
-    {
-        path: '/statistics', component: StatisticsPage
-    }
-
-]
+    children: [
+      { path: "1", components: { main: First, footer: FirstActions } },
+      { path: "2", components: { main: Second, footer: SecondActions } },
+      { path: "3", components: { main: Third, footer: ThirdActions } },
+      { path: "4", components: { main: Forth, footer: ForthActions } },
+    ],
+  },
+  { path: "/start", component: StartPage },
+  {
+    path: "/items",
+    component: ItemPage,
+    children: [
+      { path: "", component: ItemList },
+      { path: "create", component: ItemCreate },
+    ],
+  },
+  {
+    path: "/tags",
+    component: TagPage,
+    children: [
+      { path: "create", component: TagCreate },
+      { path: ":id/edit", component: TagEdit },
+    ],
+  },
+  {
+    path: "/sign_in",
+    component: SignInPage,
+  },
+  {
+    path: "/tongji",
+    component: StatisticsPage,
+  },
+];
