c6fe9dc84b25de59ce5e89047043181761dcebea
jianlong96 <liujianlong_hw_hrb@163.com>
2022-11-09 19:46:53 +0800

    获取 items 支出、收入、余额统计

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -12,6 +12,7 @@ import {
   mockTagEdit,
   mockTagIndex,
   mockTagShow,
+  mockItemIndexBalance,
 } from "../mock/mock";
 
 type GetConfig = Omit<AxiosRequestConfig, "params" | "url" | "method">;
@@ -90,6 +91,9 @@ const mock = (response: AxiosResponse) => {
       return true;
     case "itemIndex":
       [response.status, response.data] = mockItemIndex(response.config);
+      return true;
+    case "itemIndexBalance":
+      [response.status, response.data] = mockItemIndexBalance(response.config);
       return true;
   }
   return false;
