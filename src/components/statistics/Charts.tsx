3156d82da3c839b663ca5974fc6036a993dffa35
jianlong96 <liujianlong_hw_hrb@163.com>
2022-11-09 22:21:09 +0800

    切换 kind 时重新请求数据

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,4 +1,11 @@
-import { computed, defineComponent, onMounted, PropType, ref } from "vue";
+import {
+  computed,
+  defineComponent,
+  onMounted,
+  PropType,
+  ref,
+  watch,
+} from "vue";
 import { FormItem } from "../../shared/Form";
 import s from "./Charts.module.scss";
 import { LineChart } from "./LineChart";
@@ -46,7 +53,7 @@ export const Charts = defineComponent({
       });
     });
 
-    onMounted(async () => {
+    const fetchData1 = async () => {
       const response = await http.get<{ groups: Data1; summary: number }>(
         "/items/summary",
         {
@@ -58,7 +65,9 @@ export const Charts = defineComponent({
         }
       );
       data1.value = response.data.groups;
-    });
+    };
+    onMounted(fetchData1);
+    watch(() => kind.value, fetchData1);
 
     // data2
 
@@ -78,7 +87,7 @@ export const Charts = defineComponent({
         percent: Math.round((item.amount / total) * 100),
       }));
     });
-    onMounted(async () => {
+    const fetchData2 = async () => {
       const response = await http.get<{ groups: Data2; summary: number }>(
         "/items/summary",
         {
@@ -90,7 +99,9 @@ export const Charts = defineComponent({
         }
       );
       data2.value = response.data.groups;
-    });
+    };
+    onMounted(fetchData2);
+    watch(() => kind.value, fetchData2);
 
     return () => (
       <div class={s.wrapper}>
